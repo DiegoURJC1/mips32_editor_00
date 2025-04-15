99c8e46a37a965445ca0dd0a163407022526c8ee
DiegoURJC1 <d.gil.2021@alumnos.urjc.es>
2025-04-15 16:37:18 +0200

    Revert no drag for CustomNodeToolbar.jsx

--- src/flows/mips/nodes/common/node-toobar/CustomNodeToolbar.jsx
+++ src/flows/mips/nodes/common/node-toobar/CustomNodeToolbar.jsx
@@ -23,92 +23,89 @@ export default function CustomNodeToolbar(props) {
             align={"start"}
             position={props.position || Position.Right}
         >
-            <div className={"nodrag"}>
-                <div className={"custom-node-toolbar"}>
-                    {/* Pestañas */}
-                    <div className="tabs">
+            <div className={"custom-node-toolbar"}>
+                {/* Pestañas */}
+                <div className="tabs">
+                    <button
+                        className={activeTab === 'content' ? 'active' : ''}
+                        onClick={() => setActiveTab('content')}
+                    >
+                        Contenido
+                    </button>
+                    <button
+                        className={activeTab === 'handles' ? 'active' : ''}
+                        onClick={() => setActiveTab('handles')}
+                    >
+                        Handles
+                    </button>
+                    <button
+                        className={activeTab === 'extra' ? 'active' : ''}
+                        onClick={() => setActiveTab('extra')}
+                    >
+                        Extra
+                    </button>
+                </div>
+
+                {/* Mostrar contenido según la pestaña activa */}
+                {activeTab === 'content' ? (
+                    <div className="content-tab">
+                        <div className={"node-toolbar-section-title"}>Info básica</div>
+                        <div>{props.data?.label || 'No label provided'}</div>
+                        {props.children}
+                        <br/>
                         <button
-                            className={activeTab === 'content' ? 'active' : ''}
-                            onClick={() => setActiveTab('content')}
-                        >
-                            Contenido
-                        </button>
-                        <button
-                            className={activeTab === 'handles' ? 'active' : ''}
-                            onClick={() => setActiveTab('handles')}
-                        >
-                            Handles
-                        </button>
-                        <button
-                            className={activeTab === 'extra' ? 'active' : ''}
-                            onClick={() => setActiveTab('extra')}
-                        >
-                            Extra
-                        </button>
+                            className={"delete"}
+                            onClick={handleDelete}
+                            disabled={props.data?.isProtected}
+                        >🗑️</button>
                     </div>
 
-                    {/* Mostrar contenido según la pestaña activa */}
-                    {activeTab === 'content' ? (
-                        <div className="content-tab">
-                            <div className={"node-toolbar-section-title"}>Info básica</div>
-                            <div>{props.data?.label || 'No label provided'}</div>
-                            {props.children}
-                            <br/>
-                            <button
-                                className={"delete"}
-                                onClick={handleDelete}
-                                disabled={props.data?.isProtected}
-                            >🗑️</button>
+                ) : activeTab === 'handles' ? (
+                    <div className="handles-tab">
+                        <div className="target-handles">
+                            <div className={"node-toolbar-section-title"}>Entradas</div>
+                            {targetHandles && targetHandles.length > 0 ? (
+                                <ul>
+                                    {targetHandles.map(handle => (
+                                        <li key={handle.id}>
+
+                                            {handle.name || handle.label}
+                                            <br/>  Bits: {handle.bits || 'Nan'}
+                                            <br/>  Num: {handle.connectioncount || "∞"}
+
+                                        </li>
+                                    ))}
+                                </ul>
+                            ) : (
+                                <p>No hay handles de tipo target.</p>
+                            )}
                         </div>
+                        <div className="source-handles">
+                            <div className={"node-toolbar-section-title"}>Salidas</div>
+                            {sourceHandles && sourceHandles.length > 0 ? (
+                                <ul>
+                                    {sourceHandles.map(handle => (
+                                        <li key={handle.id}>
 
-                    ) : activeTab === 'handles' ? (
-                        <div className="handles-tab">
-                            <div className="target-handles">
-                                <div className={"node-toolbar-section-title"}>Entradas</div>
-                                {targetHandles && targetHandles.length > 0 ? (
-                                    <ul>
-                                        {targetHandles.map(handle => (
-                                            <li key={handle.id}>
+                                            {handle.name || handle.label}
+                                            <br/>  Bits: {handle.bits || 'Nan'}
+                                            <br/>  Num: {handle.connectioncount || "∞"}
 
-                                                {handle.name || handle.label}
-                                                <br/>  Bits: {handle.bits || 'Nan'}
-                                                <br/>  Num: {handle.connectioncount || "∞"}
-
-                                            </li>
-                                        ))}
-                                    </ul>
-                                ) : (
-                                    <p>No hay handles de tipo target.</p>
-                                )}
-                            </div>
-                            <div className="source-handles">
-                                <div className={"node-toolbar-section-title"}>Salidas</div>
-                                {sourceHandles && sourceHandles.length > 0 ? (
-                                    <ul>
-                                        {sourceHandles.map(handle => (
-                                            <li key={handle.id}>
-
-                                                {handle.name || handle.label}
-                                                <br/>  Bits: {handle.bits || 'Nan'}
-                                                <br/>  Num: {handle.connectioncount || "∞"}
-
-                                            </li>
-                                        ))}
-                                    </ul>
-                                ) : (
-                                    <p>No hay handles de tipo source.</p>
-                                )}
-                            </div>
+                                        </li>
+                                    ))}
+                                </ul>
+                            ) : (
+                                <p>No hay handles de tipo source.</p>
+                            )}
                         </div>
-                    ) : (
-                        <div className="extra-tab">
-                            <div className={"node-toolbar-section-title"}>Extra</div>
-                            <p>Este es el contenido extra que se muestra al seleccionar la pestaña "Extra Info".</p>
-                        </div>
-                    )}
-                </div>
+                    </div>
+                ) : (
+                    <div className="extra-tab">
+                        <div className={"node-toolbar-section-title"}>Extra</div>
+                        <p>Este es el contenido extra que se muestra al seleccionar la pestaña "Extra Info".</p>
+                    </div>
+                )}
             </div>
-
         </NodeToolbar>
     );
 }
