c38093a92a62f767a4c7711c6e369c5e80f9c973
DiegoURJC1 <d.gil.2021@alumnos.urjc.es>
2025-06-15 10:57:21 +0200

    Download images has different names

--- src/flows/common/downloadImage.js
+++ src/flows/common/downloadImage.js
@@ -1,55 +1,52 @@
 import { toPng } from 'html-to-image';
-import {getNodesBounds} from "@xyflow/react";
-import {themes} from "../../common-data/settings.js";
+import { getNodesBounds } from '@xyflow/react';
+import { themes } from '../../common-data/settings.js';
+
 const image = {
     width: 3840,
     height: 2160,
 };
 
-export function downloadImage(dataUrl) {
+function getImageName(currentPanel) {
+    if (currentPanel === 0) return 'mips32_diagram.png';
+    if (currentPanel === 1) return 'states_diagram.png';
+    return 'diagram.png'; // Valor por defecto
+}
+
+function getBackgroundColor(colorMode) {
+    if (colorMode === themes.dark) return '#232323';
+    if (colorMode === themes.light) return 'white';
+    return 'white'; // Valor por defecto
+}
+
+export function downloadImage(dataUrl, currentPanel) {
     const a = document.createElement('a');
-    a.setAttribute('download', 'mips32.png');
+    a.setAttribute('download', getImageName(currentPanel));
     a.setAttribute('href', dataUrl);
     a.click();
 }
 
 export function handleDownload(getNodes, getViewportForBounds, colorMode, currentPanel) {
     const nodesBounds = getNodesBounds(getNodes());
-    let viewport;
-    if (currentPanel === 0) {
-        viewport = getViewportForBounds(
-            nodesBounds,
-            image.width,
-            image.height,
-            2,
-            4,
-            10
-        );
-    } else if (currentPanel === 1) {
-        viewport = getViewportForBounds(
-            nodesBounds,
-            image.width,
-            image.height,
-            2.5,
-            4,
-            10
-        );
-    }
 
+    const zoom = currentPanel === 0 ? 2 : 2.5;
+    const viewport = getViewportForBounds(
+        nodesBounds,
+        image.width,
+        image.height,
+        zoom,
+        4,
+        10
+    );
 
-    let backgroundColor;
-    if (colorMode === themes.dark) {
-        backgroundColor = '#232323';
-    } else if (colorMode === themes.light) {
-        backgroundColor = 'white';
-    }
+    const backgroundColor = getBackgroundColor(colorMode);
 
     toPng(document.querySelector('.react-flow__viewport'), {
-        backgroundColor: backgroundColor,
+        backgroundColor,
         width: image.width,
         height: image.height,
         style: {
             transform: `translate(${viewport.x}px, ${viewport.y}px) scale(${viewport.zoom})`,
         },
-    }).then(downloadImage);
+    }).then((dataUrl) => downloadImage(dataUrl, currentPanel));
 }
